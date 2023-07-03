121bba3ca24e2466db8de0429745f173e3502b61
Lucy Kepner <kkepner14@gmail.com>
2023-07-02 19:45:55 -0600

    Start to build rose

--- avalancheRose.js
+++ avalancheRose.js
@@ -1,166 +1,225 @@
-let avalancheRose = new Chart('bar-chart', {
-    type: 'bar',
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                text: 'Accidents by Month',
-                padding: {
-                    top: 10,
-                    bottom: 10
-                }
-            },
-            legend: false
-        },
-        maintainAspectRatio: false,
-        onClick : evt=> {
-                const points = barChart.getElementsAtEventForMode(evt, 'nearest', { intersect: true }, true);
-                if (points.length) {
-                    monthSelected = 1;
-                    const clickedPoint = points[0];
-                    month = barChart.data.labels[clickedPoint.index];
-                    const value = barChart.data.datasets[clickedPoint.datasetIndex].data[clickedPoint.index];
-                    const monthLabels = barChart.options.scales.x.ticks;
-                    const dataset = barChart.data.datasets[0];
-                    dataset.borderWidth = [];
-                    dataset.borderColor = [];
-                    for (let i = 0; i < dataset.data.length; i++) {
-                        if (clickedPoint.index == i) {
-                            dataset.borderWidth[i] = 4;
-                            dataset.borderColor[i] = "black";
-                        } else {
-                            dataset.borderWidth[i] = 1;
-                            dataset.borderColor[i] = "rgba(0, 0, 0, 0.1)";
-                        }
-                    }
-                    if (actSelected==0 && modeSelected==0 && firstSelect==0) {
-                        firstSelect= "month";
-                        titleText="1st Filter";
-                        barLabels.length=0;
-                        updateActivityChart();
-                        barChartModeArr.length=0;
-                        processBarChartMode();
-                        }
-                    else if (firstSelect == "month") {
-                        titleText="1st Filter";
-                        actSelected=0;
-                        activity=0;
-                        modeSelected=0;
-                        mode=0;
-                        secondSelect=0;
-                        thirdSelect=0;
-                        barLabels.length=0;
-                        updateActivityChart();
-                        barChartModeArr.length=0;
-                        processBarChartMode();
-                        }
-                    else if (secondSelect==0 && firstSelect=="activity")  {
-                        secondSelect="month";
-                        titleText="2nd Filter";
-                        barChartModeArr.length=0;
-                        processBarChartMode();
-                        }
-                    else if (secondSelect==0 && firstSelect=="mode")  {
-                        secondSelect="month";
-                        titleText="2nd Filter";
-                        barLabels.length=0;
-                        updateActivityChart();
-                        }
-                    else if (secondSelect=="month" && firstSelect=="mode")  {
-                        titleText="2nd Filter";
-                        barLabels.length=0;
-                        updateActivityChart();
-                        thirdSelect=0;
-                        activity=0;
-                        actSelected=0;
-                        }
-                        
-                    else if (secondSelect=="month" && firstSelect=="activity") {
-                        titleText="2nd Filter";
-                        barChartModeArr.length=0;
-                        processBarChartMode();
-                        thirdSelect=0;
-                        mode=0;
-                        modeSelected=0;
-                        }
-                    else {
-                        thirdSelect="month";
-                        titleText="3rd Filter";
-                        }
-                    console.log(activity, month, mode, firstSelect, secondSelect, thirdSelect,actSelected, monthSelected, modeSelected);
-                    barChart.options.plugins.title.text = "Accidents by Month | "+titleText+": "+value;
-                    barChart.update();
-                    mapChange();
 
-                
-              }},
-        scales: {
-            x: {
-                title:{
-                    text: 'Month',
-                    display: false
-                    }
-            },
-                    
-            y: {
-                title:{
-                    text: 'Accidents',
-                    display: true
-                    },
-                ticks: {callback: function(value) {if (value % 1 === 0) {return value;}}}
-            }				
-                
-        }
-    },
-    data: {
-        datasets: [
-            {
-            backgroundColor:'#cdced6'
-    }
-        ]
-    }
-    
+// avy rose
+d3.selectAll(".avy-rose-part")
+.on("mouseover", function () {
+    d3.select(this).classed('hovered', true);
+})
+.on("mouseout", function () {
+    d3.select(this).classed('hovered', false);
+})
+.on('click', function () {
+    // toggle the filter on the chart
+    charts.rosePieChart.filter([$(this).attr('filter')]);
+    dc.redrawAll();
 });
 
-let barChartDates = [];
+// rose multifilters
+$('.rose-multiselector').click(el => {
+// remove all the existing filters on the rose
+charts.rosePieChart.filterAll();
 
-async function processBarChartDates() {
-    await delay (450);
-    /* console.log("barChartDates: ", barChartDates); */
-    /* Reduce the array of all accident dates  to an object with counts of unique months */
-    let dateData = barChartDates.reduce((allDates, month) => {
-            const currentCount = allDates[month] ?? 0;
-            return {
-            ...allDates,
-            [month]: currentCount + 1,
-            };
-    }, {});
+// get the filters to be applied from the element's filter attr
+let filter = JSON.parse($(el.target).attr('filter') || '[]');
 
+// apply and redraw
+charts.rosePieChart.filter([filter]);
+dc.redrawAll();
+});
 
-    /* Convert the dates and counts to array and sort by month */
-    let dateDataArr=Object.entries(dateData);
-    sortByMonth(dateDataArr);
-    function sortByMonth(arr) {
-        var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        arr.sort(function(a, b){
-        return months.indexOf(a[0]) - months.indexOf(b[0]);
-    });
-    }
-    let barChartKeys = Object.keys(Object.fromEntries(dateDataArr));
-    barChartValues=Object.values(Object.fromEntries(dateDataArr));
-    
-    /* // Update the bar chart with the labels and values */
-    barChart.data.labels = barChartKeys;
-    barChart.data.datasets[0].data=barChartValues;
-    
-    // Reset highlighting of previously selected month
-    const dataset = barChart.data.datasets[0];
-        dataset.borderWidth = [];
-        dataset.borderColor = [];
-        for (let i = 0; i < dataset.data.length; i++) {
-            dataset.borderWidth[i] = 1;
-            dataset.borderColor[i] = "rgba(0, 0, 0, 0.1)";
-            }
-    barChart.options.plugins.title.text = "Accidents by Month";
-    barChart.update();
+// sets the rose's values based on the current filters
+function _setAvyRoseValues () {
+// get the filtered values from the hidden rose chart
+var rosePieVals = charts.rosePieChart.group().top(Infinity);
+
+// iterate through valid rose keys, and if we have a dc value for them plot them on the rose. otherwise, 0.
+ROSE_KEYS.forEach(key => {
+    var pieVal = rosePieVals.find(groupedVal => groupedVal.key === key) || { value: 0 };
+    $(`[filter='${key}']`).find('text').text(pieVal.value || '');
+});
 }
+// fire it the first time
+_setAvyRoseValues();
+// and also update the avy rose whenever other filters update
+charts.ndx.onChange(_setAvyRoseValues);
+
+// attach event listener to each chart to show/hide filter elsewhere in the DOM
+dc.chartRegistry.list().forEach(function (chart) {
+$(chart.anchor()).closest('.chart-wrapper').find('.reset').click(() => {
+    chart.filterAll();
+    dc.redrawAll();
+});
+
+chart.on('filtered', function () {
+    if (chart.hasFilter()) {
+        $(chart.anchor()).closest('.chart-wrapper').find('.reset').removeClass('hidden');
+    } else {
+        $(chart.anchor()).closest('.chart-wrapper').find('.reset').addClass('hidden');
+    }
+
+    geo.clearPopup();
+
+    // update highlighted parts of the rose automatically when it gets filtered
+    // we only get one event listener per chart so we have to bundle it in here.
+    if (chart.anchorName() === 'rose-pie-chart') {
+        var filters = charts.rosePieChart.filters();
+
+        // if there are no filters, we want nothing to show as not selected
+        if (filters.length === 0) {
+            $('.avy-rose-part').removeClass('not-selected');
+        } else {
+            // otherwise, there are filters to apply so we first deselect all elements...
+            $('.avy-rose-part').addClass('not-selected');
+
+            // and then remove this deselected class from the ones that are indeed selected.
+            filters.forEach(filter => {
+                $(`[filter='${filter}']`).removeClass('not-selected');
+            });
+        }
+    }
+
+    // if zones are filtered via the bar chart, zoom to zone bounds
+    if (chart.anchorName() === 'zone-bar-chart') {
+        if (chart.filters().length) {
+            // zoom to the selected zone bounds
+            var selectedZones = chart.filters();
+            var bboxes = Object.values(ZONES).filter(zone => selectedZones.includes(zone.name)).map(zone => zone.bbox);
+            var megaBbox = turf.bbox(turf.featureCollection(bboxes.map(bbox => turf.bboxPolygon(bbox))));
+            geo.map.fitBounds(megaBbox);
+        } else {
+            // return to initial bounds
+            geo.map.fitBounds(ZONES_BOUNDS);
+        }
+    }
+
+    // update the data displayed by all layers in the map to only include the data we've filtered to
+    var selectedFeaturesGeojson = utils.getEmptyGeoJson();
+    selectedFeaturesGeojson.features = filteredObjs.top(Infinity);
+    geo.filterToFeatures(selectedFeaturesGeojson);
+    charts.obsDataTable.updatePagination(1); // reset to page 1 whenever filtered, staying on page X holds no meaning
+
+    // if there's only one feature filtered, open the info box automatically
+    if (selectedFeaturesGeojson.features.length == 1) {
+        geo.makePopup(null, selectedFeaturesGeojson.features[0].properties.id);
+    }
+});
+});
+
+// render all charts, then draw the date selector for future date changes.
+$('#loading-overlay').toggleClass('hidden', true)
+dc.renderAll();
+charts._renderDateSelector();
+}
+
+
+};
+// avy rose
+d3.selectAll(".avy-rose-part")
+.on("mouseover", function () {
+    d3.select(this).classed('hovered', true);
+})
+.on("mouseout", function () {
+    d3.select(this).classed('hovered', false);
+})
+.on('click', function () {
+    // toggle the filter on the chart
+    charts.rosePieChart.filter([$(this).attr('filter')]);
+    dc.redrawAll();
+});
+
+// rose multifilters
+$('.rose-multiselector').click(el => {
+// remove all the existing filters on the rose
+charts.rosePieChart.filterAll();
+
+// get the filters to be applied from the element's filter attr
+let filter = JSON.parse($(el.target).attr('filter') || '[]');
+
+// apply and redraw
+charts.rosePieChart.filter([filter]);
+dc.redrawAll();
+});
+
+// sets the rose's values based on the current filters
+function _setAvyRoseValues () {
+// get the filtered values from the hidden rose chart
+var rosePieVals = charts.rosePieChart.group().top(Infinity);
+
+// iterate through valid rose keys, and if we have a dc value for them plot them on the rose. otherwise, 0.
+ROSE_KEYS.forEach(key => {
+    var pieVal = rosePieVals.find(groupedVal => groupedVal.key === key) || { value: 0 };
+    $(`[filter='${key}']`).find('text').text(pieVal.value || '');
+});
+}
+// fire it the first time
+_setAvyRoseValues();
+// and also update the avy rose whenever other filters update
+charts.ndx.onChange(_setAvyRoseValues);
+
+// attach event listener to each chart to show/hide filter elsewhere in the DOM
+dc.chartRegistry.list().forEach(function (chart) {
+$(chart.anchor()).closest('.chart-wrapper').find('.reset').click(() => {
+    chart.filterAll();
+    dc.redrawAll();
+});
+
+chart.on('filtered', function () {
+    if (chart.hasFilter()) {
+        $(chart.anchor()).closest('.chart-wrapper').find('.reset').removeClass('hidden');
+    } else {
+        $(chart.anchor()).closest('.chart-wrapper').find('.reset').addClass('hidden');
+    }
+
+    geo.clearPopup();
+
+    // update highlighted parts of the rose automatically when it gets filtered
+    // we only get one event listener per chart so we have to bundle it in here.
+    if (chart.anchorName() === 'rose-pie-chart') {
+        var filters = charts.rosePieChart.filters();
+
+        // if there are no filters, we want nothing to show as not selected
+        if (filters.length === 0) {
+            $('.avy-rose-part').removeClass('not-selected');
+        } else {
+            // otherwise, there are filters to apply so we first deselect all elements...
+            $('.avy-rose-part').addClass('not-selected');
+
+            // and then remove this deselected class from the ones that are indeed selected.
+            filters.forEach(filter => {
+                $(`[filter='${filter}']`).removeClass('not-selected');
+            });
+        }
+    }
+
+    // if zones are filtered via the bar chart, zoom to zone bounds
+    if (chart.anchorName() === 'zone-bar-chart') {
+        if (chart.filters().length) {
+            // zoom to the selected zone bounds
+            var selectedZones = chart.filters();
+            var bboxes = Object.values(ZONES).filter(zone => selectedZones.includes(zone.name)).map(zone => zone.bbox);
+            var megaBbox = turf.bbox(turf.featureCollection(bboxes.map(bbox => turf.bboxPolygon(bbox))));
+            geo.map.fitBounds(megaBbox);
+        } else {
+            // return to initial bounds
+            geo.map.fitBounds(ZONES_BOUNDS);
+        }
+    }
+
+    // update the data displayed by all layers in the map to only include the data we've filtered to
+    var selectedFeaturesGeojson = utils.getEmptyGeoJson();
+    selectedFeaturesGeojson.features = filteredObjs.top(Infinity);
+    geo.filterToFeatures(selectedFeaturesGeojson);
+    charts.obsDataTable.updatePagination(1); // reset to page 1 whenever filtered, staying on page X holds no meaning
+
+    // if there's only one feature filtered, open the info box automatically
+    if (selectedFeaturesGeojson.features.length == 1) {
+        geo.makePopup(null, selectedFeaturesGeojson.features[0].properties.id);
+    }
+});
+});
+
+// render all charts, then draw the date selector for future date changes.
+$('#loading-overlay').toggleClass('hidden', true)
+dc.renderAll();
+charts._renderDateSelector();
+}
+};
