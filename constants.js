c20a193222c6f833029eb4360f5b1e3e5b72d2ee
dseidlCoMtn <71347225+dseidlCoMtn@users.noreply.github.com>
2023-08-31 11:42:12 -0600

    Update client id
    Update client id

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
 const AUTH0_CONFIG = {
     domain: 'caic.auth0.com',
-    client_id: 'LxKDi5svRM7TI0W3LxyjFCD4xqWUn5Zn',
+    //client_id: 'LxKDi5svRM7TI0W3LxyjFCD4xqWUn5Zn',
+    client_id: 'JBIMQ2zJLh9dQqJZnqagYC7XRUCg85z4',
     //audience: 'https://api.avalanche.state.co.us'
 };
